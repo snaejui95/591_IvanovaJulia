d9e66f8026a4503a1323249a7e6a52d58acf8ee8
snaejui95 <73231782+snaejui95@users.noreply.github.com>
2022-01-20 22:34:29 +0300

    Add files via upload

---
+++ HelloApp/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+
+        double a = 3, b = 4, c;
+        c = Math.sqrt(a * a + b * b);
+        System.out.println(c);
+    }
+}
